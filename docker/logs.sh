4e0ac4ac69e49d6ab9cbc41ced1077921e4d43b7
Rémi <rcocula@gmail.com>
2025-03-29 11:17:22 +0100

    WIP

---
+++ docker/logs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+COMPOSE_FILE=docker-compose.yml
+if [ ${1} == "dev" ]; then
+  COMPOSE_FILE=docker-compose-dev.yml
+fi
+
+
+docker-compose -f ${COMPOSE_FILE} --project-name nextcloud-${1} --env-file .env.${1} logs nextcloud
---
+++ docker/restart.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+COMPOSE_FILE=docker-compose.yml
+if [ ${1} == "dev" ]; then
+  COMPOSE_FILE=docker-compose-dev.yml
+fi
+
+
+docker-compose -f ${COMPOSE_FILE} --project-name nextcloud-${1} --env-file .env.${1} stop nextcloud
+
+docker-compose -f ${COMPOSE_FILE} --project-name nextcloud-${1} --env-file .env.${1} up nextcloud
