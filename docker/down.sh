84427fb77cb20fe3168047e14263cdd96e0d2ebf
Rémi <glatech@gmx.fr>
2025-05-02 11:56:23 +0200

    WIP

--- docker/down.sh
+++ docker/down.sh
@@ -5,4 +5,4 @@ if [ ${1} == "dev" ]; then
   COMPOSE_FILE=docker-compose-dev.yml
 fi
 
-docker-compose -f ${COMPOSE_FILE} --project-name nextcloud-${1} --env-file .env.${1} down
+docker-compose -f ${COMPOSE_FILE} --project-name nextcloud-${1} --env-file .env.${1} down -v
--- docker/nc-bash.sh
+++ docker/nc-bash.sh
@@ -2,7 +2,7 @@
 
 #echo `docker ps | grep -E 'nextcloud-.+_nextcloud_1' | sed 's/nextcloud-\(.+\)_nextcloud/\1/'`
 
-NC_CONTAINER=`docker ps --format '{{.Names}}' | grep -E 'nextcloud-.+_nextcloud_1'`
+NC_CONTAINER=`docker ps --format '{{.Names}}' | grep -E 'nextcloud-.+[_-]nextcloud[_-]1'`
 
 echo ${NC_CONTAINER}
 
